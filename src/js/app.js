35f57bc23e7bcf6d6d607ba8f62175a9095a0528
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-11-30 23:19:50 +0900

    CartItemコンポーネントを表示

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './hello-world';
+import CartItem from './cartItem';
+
+const order = {
+  title: 'Fresh fruits package',
+  image: 'http://images.all-free-download.com/images/graphiclarge/citrus_fruit_184416.jpg',
+  initialQty: 3,
+  price: 8
+};
 
 ReactDOM.render(
-  <HelloWorld phrase="ES6" />,
+  <CartItem title={order.title}
+    image={order.image}
+    initialQty={order.initialQty}
+    price={order.price} />,
   document.getElementById('app')
 );
--- src/js/hello-world.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class HelloWorld extends React.Component {
-  render() {
-    return <h1>Hello from {this.props.phrase}!</h1>
-  }
-}
